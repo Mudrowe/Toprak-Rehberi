d9c63a52d81da2e971d8a11e4db93210fb630bf7
Mete Uysal <meteuysal32@gmail.com>
2024-08-08 15:07:30 +0300

    frontend: adjust charts
    Cards and charts aren't recieved dynamically. So, stats are not correct

--- lib/features/main_pages/home/widgets/pie_chart.dart
+++ lib/features/main_pages/home/widgets/pie_chart.dart
@@ -4,15 +4,20 @@ import 'package:toprak_rehberi/utils/constants/colors.dart';
 
 class TPieChart extends StatelessWidget {
   final String chartName;
+  final List<PieChartSectionData> sections;
+  final List<LegendItem> legendItems;
 
-  const TPieChart({super.key, required this.chartName});
-
+  const TPieChart(
+      {super.key,
+      required this.chartName,
+      required this.sections,
+      required this.legendItems});
 
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-        aspectRatio: 1.3,
-        child: Row(  
+        aspectRatio: 1.5,
+        child: Row(
           children: [
             Expanded(
               child: Stack(alignment: Alignment.center, children: [
@@ -24,59 +29,22 @@ class TPieChart extends StatelessWidget {
                       fontSize: 15),
                 ),
                 PieChart(
-                  PieChartData(sections: [
-                    PieChartSectionData(
-                        value: 25,
-                        showTitle: false,
-                        color: TColors.pieChartColor1,
-                        title: '25%'),
-                    PieChartSectionData(
-                        value: 29,
-                        showTitle: false,
-                        color: TColors.pieChartColor2,
-                        title: '%29'),
-                    PieChartSectionData(
-                      value: 28,
-                      showTitle: false,
-                      color: TColors.pieChartColor3,
-                    ),
-                    PieChartSectionData(
-                        value: 38,
-                        showTitle: false,
-                        color: TColors.pieChartColor4,
-                        title: '%38'),
-                  ], centerSpaceRadius: 90),
+                  PieChartData(
+                      sections: sections.map((section) {
+                        return section;
+                      }).toList(),
+                      centerSpaceRadius: 90),
                 ),
               ]),
             ),
-            
-            const Padding(
-              padding: EdgeInsets.only(right: 20.0),
+            Padding(
+              padding: const EdgeInsets.only(right: 20.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  LegendItem(
-                    color: TColors.pieChartColor1,
-                    productName: 'Patates',
-                    percentage: '25%',
-                  ),
-                  LegendItem(
-                    color: TColors.pieChartColor2,
-                    productName: 'Buğday',
-                    percentage: '29%',
-                  ),
-                  LegendItem(
-                    color: TColors.pieChartColor3,
-                    productName: 'Havuç',
-                    percentage: '28%',
-                  ),
-                  LegendItem(
-                    color: TColors.pieChartColor4,
-                    productName: 'Çilek',
-                    percentage: '38%',
-                  ),
-                ],
+                children: legendItems.map((legendItem) {
+                  return legendItem;
+                }).toList(),
               ),
             )
           ],
@@ -107,9 +75,7 @@ class LegendItem extends StatelessWidget {
             height: 10,
           ),
           const SizedBox(width: 8),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start, 
-            children: [
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
             Text(
               productName,
               style: const TextStyle(
