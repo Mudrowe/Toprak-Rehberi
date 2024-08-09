c7488057f2d3dd8f4c7c8f9038053e3d2ea30643
Mete Uysal <meteuysal32@gmail.com>
2024-08-09 08:55:39 +0300

    frontend: change score color

--- lib/features/main_pages/products/widgets/score_display.dart
+++ lib/features/main_pages/products/widgets/score_display.dart
@@ -18,38 +18,31 @@ class TScoreDisplay extends StatelessWidget {
   Widget build(BuildContext context) {
     String imagePath;
     String scoreText;
-    Color scoreColor;
 
     switch (score) {
       case 1:
         imagePath = TImages.bad; 
         scoreText = 'Kötü';
-        scoreColor = TColors.bad;
         break;
       case 2:
         imagePath = TImages.meh;
         scoreText = 'Eh İşte';
-        scoreColor = TColors.meh;
         break;
       case 3:
         imagePath = TImages.mid;
         scoreText = 'Ortalama';
-        scoreColor = TColors.mid;
         break;
       case 4:
         imagePath = TImages.good;
         scoreText = 'İyi';
-        scoreColor = TColors.good;
         break;
       case 5:
         imagePath = TImages.veryGood;
         scoreText = 'Çok İyi';
-        scoreColor = TColors.veryGood;
         break;
       default:
         imagePath = TImages.mid; 
         scoreText = 'Ortalama';
-        scoreColor = TColors.mid;
     }
 
     return Column(
@@ -62,10 +55,10 @@ class TScoreDisplay extends StatelessWidget {
         ),
         Text(
           scoreText,
-          style: TextStyle(
+          style: const TextStyle(
             fontWeight: FontWeight.bold,
-            color: scoreColor, 
-            fontSize: 22
+            fontSize: 22,
+            color: TColors.black
           ),
         ),
       ],
