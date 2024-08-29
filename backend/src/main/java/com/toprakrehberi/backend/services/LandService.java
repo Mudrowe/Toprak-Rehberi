d5e5b93aae952e688b634a586175542577e8eba4
Mete Uysal <meteuysal32@gmail.com>
2024-08-29 10:32:56 +0300

    backend: add services

---
+++ backend/src/main/java/com/toprakrehberi/backend/services/LandService.java
@@ -0,0 +1,16 @@
+package com.toprakrehberi.backend.services;
+
+import com.toprakrehberi.backend.models.Land;
+import com.toprakrehberi.backend.repositories.LandRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LandService {
+    @Autowired
+    private LandRepository landRepository;
+
+    public List<Land> getAllLands() { return landRepository.findAll(); }
+}
